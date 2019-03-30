eb1ab4f7d73fb643d83b39edea874a0070cf2b72
Igor Plotnikov <ig.plotnikov@gmail.com>
2019-03-30 13:12:53 +0300

    drugs controllers added

---
+++ app/controllers/drugs_controller.rb
@@ -0,0 +1,50 @@
+class DrugsController < ApplicationController
+  before_action :set_drug, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @drugs = Drug.all
+  end
+
+  def show
+  end
+
+  def new
+    @drug = Drug.new
+  end
+
+  def edit
+  end
+
+  def create
+    @drug = Drug.new(drug_params)
+
+    if @drug.save
+      redirect_to @drug, notice: 'Drug was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @drug.update(drug_params)
+      redirect_to @drug, notice: 'Drug was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @drug.destroy
+
+    redirect_to drugs_url, notice: 'Drug was successfully destroyed.'
+  end
+
+  private
+    def set_drug
+      @drug = Drug.find(params[:id])
+    end
+
+    def drug_params
+      params.require(:drug).permit(:id))
+    end
+end
--- app/controllers/request_drugs_controller.rb
+++ app/controllers/request_drugs_controller.rb
@@ -1,74 +1,51 @@
 class RequestDrugsController < ApplicationController
   before_action :set_request_drug, only: [:show, :edit, :update, :destroy]
 
-  # GET /request_drugs
-  # GET /request_drugs.json
   def index
     @request_drugs = RequestDrug.all
   end
 
-  # GET /request_drugs/1
-  # GET /request_drugs/1.json
   def show
   end
 
-  # GET /request_drugs/new
   def new
     @request_drug = RequestDrug.new
   end
 
-  # GET /request_drugs/1/edit
   def edit
   end
 
-  # POST /request_drugs
-  # POST /request_drugs.json
   def create
     @request_drug = RequestDrug.new(request_drug_params)
 
-    respond_to do |format|
-      if @request_drug.save
-        format.html { redirect_to @request_drug, notice: 'Request drug was successfully created.' }
-        format.json { render :show, status: :created, location: @request_drug }
-      else
-        format.html { render :new }
-        format.json { render json: @request_drug.errors, status: :unprocessable_entity }
-      end
+    if @request_drug.save
+      redirect_to @request_drug, notice: 'Request drug was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /request_drugs/1
-  # PATCH/PUT /request_drugs/1.json
   def update
-    respond_to do |format|
-      if @request_drug.update(request_drug_params)
-        format.html { redirect_to @request_drug, notice: 'Request drug was successfully updated.' }
-        format.json { render :show, status: :ok, location: @request_drug }
-      else
-        format.html { render :edit }
-        format.json { render json: @request_drug.errors, status: :unprocessable_entity }
-      end
+    if @request_drug.update(request_drug_params)
+      redirect_to @request_drug, notice: 'Request drug was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /request_drugs/1
-  # DELETE /request_drugs/1.json
   def destroy
     @request_drug.destroy
-    respond_to do |format|
-      format.html { redirect_to request_drugs_url, notice: 'Request drug was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to request_drugs_url, notice: 'Request drug was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_request_drug
       @request_drug = RequestDrug.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def request_drug_params
-      params.fetch(:request_drug, {})
+      params.require(:request_drug).permit(:id)
     end
 end
