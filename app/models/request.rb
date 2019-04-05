1d85adc99b776af323da5c7a78516487a36fb2c9
Igor <ig.plotnikov@gmail.com>
2019-04-05 16:33:05 +0300

    destroy requests updated

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
-  has_many :request_drugs
+  has_many :request_drugs, dependent: :delete_all
   has_many :drugs, through: :request_drugs
 
   validates :auction_number, presence: true
